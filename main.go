ceb90e73100f1eeda06148a0ea5a0ef3f7cc42fb
dgtyPedro <pedrogcs0104@gmail.com>
2024-01-18 15:43:48 -0300

    refactor: remove fyne da aplicação

--- main.go
+++ main.go
@@ -1,86 +1,71 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/layout"
-	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
-	// "fyne.io/fyne/v2/dialog"
+	"github.com/nguyenthenguyen/docx"
 	"github.com/sqweek/dialog"
-	"image/color"
-	"net/url"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 )
 
-type Navigator struct {
-	step     int
-	lastStep *int
-	window   fyne.Window
-}
-
-var tailor = app.New()
-
-var navigator = &Navigator{
-	step:     1,
-	lastStep: nil,
-	window:   tailor.NewWindow("Keyword Tailor"),
-}
-
-func loadFileInput() *fyne.Container {
-	inputButton := widget.NewButton("Select your document to start", func() {
-		filename, _ := dialog.File().Title("Select your document").Load()
-		fmt.Println(filename)
-		renderStep(2)
-	})
-	title := canvas.NewText("Keyword Tailor", color.White)
-	linkURL, _ := url.Parse("https://docs.google.com/document/d/1DaJguK3Wo_Z7I177fyVfvrEPU1QJZzi0Tx4IKsBDNF8/edit?usp=sharing")
-
-	template := widget.NewHyperlink("Get our recommended cv template here",
-		linkURL)
-	return container.NewVBox(
-		layout.NewSpacer(),
-		container.NewHBox(
-			layout.NewSpacer(),
-			container.NewVBox(container.NewCenter(title), inputButton),
-			layout.NewSpacer(),
-		),
-		layout.NewSpacer(),
-		container.NewCenter(template),
-	)
-}
-
-func loadFileInfo() *fyne.Container {
-	return container.NewVBox(
-		layout.NewSpacer(),
-		container.NewHBox(
-			layout.NewSpacer(),
-			widget.NewButtonWithIcon("Go Back", theme.NavigateBackIcon(), func() {
-				renderStep(1) // Voltar para a página anterior
-			}),
-			layout.NewSpacer(),
-		),
-		layout.NewSpacer(),
-	)
-}
-
-func renderStep(step int) {
-	fmt.Println(step)
-	navigator.lastStep = &navigator.step
-	navigator.step = step
-	switch step {
-	case 2:
-		navigator.window.SetContent(loadFileInfo())
-	default:
-		navigator.window.SetContent(loadFileInput())
-	}
+type KeywordPair struct {
+	keyword     string
+	replacement string
 }
 
 func main() {
-	renderStep(1)
-	myWindow := navigator.window
-	myWindow.Resize(fyne.NewSize(900, 900))
-	myWindow.ShowAndRun()
+	fmt.Println("Hello World.")
+
+	fmt.Println("How many documents you want to generate today?")
+
+	var documentCount int
+	_, err := fmt.Scanln(&documentCount)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(documentCount)
+
+	fmt.Println("Now, select your document")
+
+	filepath, _ := dialog.File().Title("Select your document").Load()
+
+	in := bufio.NewReader(os.Stdin)
+
+	file, _ := docx.ReadDocxFile(filepath)
+
+	for i := 1; i <= documentCount; i++ {
+		var kps []KeywordPair
+		fmt.Printf("Insert the keywords and replacements for file number %s (type /q to quit) \n", strconv.Itoa(i))
+		newFile := file.Editable()
+		for b := false; !b; {
+			fmt.Println("Type your keyword")
+			kw, _ := in.ReadString('\n')
+			kw = strings.TrimSpace(strings.TrimSuffix(kw, "\n"))
+			if kw == "/q" {
+				b = true
+				break
+			}
+			fmt.Println("Type your replacement")
+			rp, _ := in.ReadString('\n')
+			rp = strings.TrimSpace(strings.TrimSuffix(rp, "\n"))
+			if rp == "/q" {
+				b = true
+				break
+			}
+			kps = append(kps, KeywordPair{keyword: kw, replacement: rp})
+		}
+
+		for _, pair := range kps {
+			_ = newFile.Replace(pair.keyword, pair.replacement, -1)
+			fmt.Printf("Chave: %s, Valor: %s\n", pair.keyword, pair.replacement)
+		}
+		_ = newFile.WriteToFile("./doc" + strconv.Itoa(i) + ".docx")
+	}
+
+	fmt.Println(filepath)
+
 }
