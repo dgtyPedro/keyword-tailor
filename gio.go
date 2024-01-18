2212a630225a3a1f4f806864b6ef3bbf5ffc34dd
dgtyPedro <pedrogcs0104@gmail.com>
2024-01-18 18:09:09 -0300

    feat: começa design usando gio (gio >>> fyne)

---
+++ gio.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+	"gioui.org/app"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/unit"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+	"github.com/sqweek/dialog"
+	"image"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+type Navigator struct {
+	step int
+}
+
+var nav = Navigator{
+	step: 1,
+}
+
+func main() {
+	go func() {
+		w := app.NewWindow()
+		err := run(w)
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	}()
+	app.Main()
+}
+
+func run(w *app.Window) error {
+	th := material.NewTheme()
+	var ops op.Ops
+
+	var startButton widget.Clickable
+	var docRedirect widget.Clickable
+
+	for {
+		switch e := w.NextEvent().(type) {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewContext(&ops, e)
+
+			if startButton.Clicked(gtx) {
+				filepath, err := dialog.File().Title("Select your document").Load()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Println(filepath)
+			}
+
+			if docRedirect.Clicked(gtx) {
+				fmt.Println("redirecting")
+				var err error
+				url := "https://docs.google.com/document/d/1DaJguK3Wo_Z7I177fyVfvrEPU1QJZzi0Tx4IKsBDNF8"
+				switch runtime.GOOS {
+				case "linux":
+					err = exec.Command("xdg-open", url).Start()
+				case "windows":
+					err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+				case "darwin":
+					err = exec.Command("open", url).Start()
+				default:
+					err = fmt.Errorf("unsupported platform")
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+
+			layout.Flex{
+				Axis:      layout.Vertical,
+				Spacing:   layout.SpaceSides,
+				Alignment: layout.Middle,
+			}.Layout(gtx,
+				layout.Flexed(2,
+					func(gtx layout.Context) layout.Dimensions {
+						return layout.Flex{
+							Axis:      layout.Vertical,
+							Spacing:   layout.SpaceSides,
+							Alignment: layout.Middle,
+						}.Layout(gtx,
+							layout.Rigid(
+								func(gtx layout.Context) layout.Dimensions {
+									t := material.Body1(th, "Keyword Taylor")
+									t.Alignment = text.Middle
+									return t.Layout(gtx)
+								},
+							),
+							layout.Rigid(
+								layout.Spacer{Height: unit.Dp(25)}.Layout,
+							),
+							layout.Rigid(
+								func(gtx layout.Context) layout.Dimensions {
+									gtx.Constraints = layout.Exact(image.Pt(300, 50))
+									btn := material.Button(th, &startButton, "Insert your document to start")
+									return btn.Layout(gtx)
+								},
+							),
+						)
+					},
+				),
+				layout.Flexed(0.1,
+					func(gtx layout.Context) layout.Dimensions {
+						return layout.Stack{}.Layout(gtx,
+							layout.Stacked(
+								func(gtx layout.Context) layout.Dimensions {
+									return material.Clickable(gtx, &docRedirect, func(gtx layout.Context) layout.Dimensions {
+										return material.Subtitle1(th, "Need a cv? Use our template.").Layout(gtx)
+									})
+								},
+							),
+						)
+					},
+				),
+			)
+
+			e.Frame(gtx.Ops)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -3,8 +3,19 @@ module github.com/dgtyPedro/keyword-tailor
 go 1.21.3
 
 require (
+	gioui.org v0.4.2
 	github.com/nguyenthenguyen/docx v0.0.0-20230621112118-9c8e795a11db
 	github.com/sqweek/dialog v0.0.0-20220809060634-e981b270ebbf
 )
 
-require github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
+require (
+	gioui.org/cpu v0.0.0-20210817075930-8d6a761490d2 // indirect
+	gioui.org/shader v1.0.8 // indirect
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
+	github.com/go-text/typesetting v0.0.0-20230803102845-24e03d8b5372 // indirect
+	golang.org/x/exp v0.0.0-20221012211006-4de253d81b95 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220827204233-334a2380cb91 // indirect
+	golang.org/x/image v0.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220825204002-c680a09ffe64 // indirect
+	golang.org/x/text v0.7.0 // indirect
+)
